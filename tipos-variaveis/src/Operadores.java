897783367884f8f559f95d37794a7eeec08de3e0
Jair Sousa <jairsmarques1126@gmail.com>
2022-10-11 10:14:43 -0300

    exemplo de operador lógico

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -106,6 +106,28 @@ else
             nomeDois = new String("Jair");
             System.out.println(nomeUm.equals(nomeDois));
 
+        System.out.println("-------------------------------");
+        System.out.println("----Uso de operador lógico---");
+        System.out.println("-------------------------------");
+
+        // Operadores.java
+        boolean condicao1=true;
+
+        boolean condicao2=false;
+
+        /* Aqui estamos utilizando o operador lógico E para fazer a união de duas 
+        expressões. 
+        É como se estivesse escrito:
+        "Se Condicao1 e Condicao2 forem verdadeiras, executar código"
+        */
+
+        if(condicao1 && condicao2)
+            System.out.print("Os dois valores precisam ser verdadeiros");;
+
+        //Se condicao1 OU condicao2 for verdadeira, executar código.
+        if(condicao1 || condicao2)
+            System.out.print("Um dos valores precisa ser verdadeiro");
+
     }
     
     
