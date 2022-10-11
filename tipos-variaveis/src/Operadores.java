ec33aede73c7fed1f9d758232143dd68cd9ca4d9
Jair Sousa <jairsmarques1126@gmail.com>
2022-10-11 10:04:32 -0300

    exemplos de operadores relacional

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -73,9 +73,30 @@ else
 
         System.out.println(resultado);
 
+        System.out.println("-------------------------------");
+        System.out.println("----Uso de operador relacional---");
+        System.out.println("-------------------------------");
 
+        //classe Operadores.java
+        int numero1 = 1;
+        int numero2 = 2;
 
+        if(numero1 > numero2)
+            System.out.println("Numero 1 maior que numero 2");
+
+        if(numero1 < numero2)
+            System.out.println("Numero 1 menor que numero 2");
+
+        if(numero1 >= numero2)
+            System.out.println("Numero 1 maior ou igual que numero 2");
+
+        if(numero1 <= numero2)
+            System.out.println("Numero 1 menor ou igual que numero 2");
+
+        if(numero1 != numero2)
+            System.out.println("Numero 1 é diferente de numero 2");
 
     }
+    
     
 }
