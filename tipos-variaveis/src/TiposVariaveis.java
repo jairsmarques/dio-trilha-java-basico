3504808aef36edf65eb30f77d70307e0ece77496
Jair de Sousa <jairsmarques1126@gmail.com>
2023-09-20 17:34:32 -0300

    estudos de exmplos de tipos de variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,27 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // tipos primitivos
+        // estudem a classe String que representa texto na aplicação
+
+        String meuNome = "Jair de Sousa";
+
+        double salarioMinimo = 2500.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(meuNome);
+        System.out.println(salarioMinimo);
+        System.out.println(numeroCurto2);
+        System.out.println(VALOR_DE_PI);
+
     }
 }
