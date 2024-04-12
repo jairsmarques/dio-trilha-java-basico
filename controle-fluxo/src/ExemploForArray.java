ec9a7327d707340352d124ae2a1e73ae4a9951cf
Jair de Sousa <jairsmarques1126@gmail.com>
2024-04-12 10:13:44 -0300

    teste

--- controle-fluxo/src/ExemploForArray.java
+++ controle-fluxo/src/ExemploForArray.java
@@ -1,6 +1,6 @@
 public class ExemploForArray {
     public static void main(String[] args) {
-
+        // indice de array inicia em 0
         String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
 
 	    for (int x = 0; x < alunos.length; x++) {
